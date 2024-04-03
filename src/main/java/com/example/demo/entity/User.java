1378d3674c7b17b5da46af2af614b812c9519e4a
AdrianG12345 <georgiu.adrian10@gmail.com>
2024-04-03 12:39:44 +0300

    pot da commmit?

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,116 +2,59 @@ package com.example.demo.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.NamedQuery;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serializable;
-
-@NamedQuery(name="User.findByEmailId", query = "select u from User u where u.email=:email")
+import java.util.Collection;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@DynamicUpdate
-@DynamicInsert
-@Table(name = "user")
-public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
+@Table(name="user")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "contactNumber")
-    private String contactNumber;
-
-    @Column(name = "password")
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
     private String password;
 
-    @Column(name = "status")
-    private String status;
 
-    @Column(name = "role")
-    private String role;
-
-    @Column(name = "email")
-    private String email;
-
-    public User(int id) {
-        this.id = id;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
-    public User(String name, String contactNumber, String password, String status, String role, String email) {
-        this.name = name;
-        this.email = email;
-        this.contactNumber = contactNumber;
-        this.password = password;
-        this.status = status;
-        this.role = role;
+    @Override
+    public String getUsername() {
+        return null;
     }
 
-    public User() {
-
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
     }
 
-    public int getId() {
-        return id;
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
